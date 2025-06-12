d7565121f8c148bd8a8b42147341ff929ff763e4
kamiya <shi_taka04@icloud.com>
2025-06-12 09:09:20 +0900

    クラス名を名詞かつ先頭大文字に変更

--- Kazuate.java
+++ Kazuate.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class guessTheNumber {
+public class Kazuate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int answer = 73; // プログラマが設定する2桁の正の整数（例として73）
